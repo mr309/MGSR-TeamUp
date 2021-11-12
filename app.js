352502274a733717b8539e7a44c3993393e41bb9
Mat <mathew.rayman@gmail.com>
2021-11-12 06:40:31 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const players = [  
+  "@mr309#3290",
+  "@__Henry__#9095",
+  "@Andrew-Morse#8509",
+  "@Master Bates#4935",
+  "@Goosebumps#8209",
+  "@igonnawrecku#1386",
+  "@InvinciBeard#3473",
+  "@leftytehllama#8951",
+  "@Me, Ed#3697",
+  "@Michael722#2048",
+  "@PatsWhatImTalkinAbout#2731",
+  "@strangemusic#5017",
+  "@Rabbit#5852",
+  "@Splash#0972",
+  "@The Mario Odyssey#6494",
+  "@🐙MistahKush#5119"
+]
+
+function results(r1, r2, r3, r4) {
+    console.log(`/game record game: MGSR 1v1 result: #1 ${r1} #2 ${r2}`)
+}
+
+results(players[0], players[1])
