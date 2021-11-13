5a2fc4779951c814bb320c7b211742098dd4f468
Mat <mathew.rayman@gmail.com>
2021-11-13 15:56:28 -0500

    Added player fields, with 'Add' buttons, 'Results' box, which displays the / command for Discord

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const players = [
   "@🐙MistahKush#5119",
 ];
 
-const list = document.querySelectorAll(".players");
+const list = document.querySelector("#players");
 
 //players.forEach((item) => {
 //  let option = document.createElement("option");
@@ -31,11 +31,30 @@ const list = document.querySelectorAll(".players");
 players.forEach((item) => {
   let option = document.createElement("option");
   option.value = item;
-  list.forEach((e) => e.appendChild(option));
+  //list.forEach((e) => {
+  list.appendChild(option);
+  //});
 });
 
-function results(r1, r2, r3, r4) {
-  console.log(`/game record game: MGSR 1v1 result: #1 ${r1} #2 ${r2}`);
-}
+// function results(r1, r2, r3, r4) {
+//   console.log(`/game record game: MGSR 1v1 result: #1 ${r1} #2 ${r2}`);
+// }
 
-results(players[0], players[1]);
+// results(players[0], players[1]);
+
+function AddResult(button) {
+  let resultsBox = document.getElementById("results");
+
+  if (button.id == "1st") {
+    resultsBox.innerHTML += " #1 " + document.getElementById("firstplace").value;
+  }
+  if (button.id == "2nd") {
+    resultsBox.innerHTML += " #2 " + document.getElementById("secondplace").value;
+  }
+  if (button.id == "3rd") {
+    resultsBox.innerHTML += " #3 " + document.getElementById("thirdplace").value;
+  }
+  if (button.id == "4th") {
+    resultsBox.innerHTML += " #4 " + document.getElementById("fourthplace").value;
+  }
+}
