e9dff6e646b265e8e5d66a6d805cbf6a3065acd0
Mat <mathew.rayman@gmail.com>
2021-12-26 22:07:34 -0500

    Attempted to fix issue with async getUsers function

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ const gameState = {
 
 const defaultCommand = "/game record leaderboard: " + LEADERBOARD_NAME + " result:";
 
-const userIDs = await getUsers(LEADERBOARD_NAME);
+const userIDs = /* await */ getUsers(LEADERBOARD_NAME);
 
 // List of users, which needs updating from time to time.
 function getUserData(leaderboardName) {
@@ -302,7 +302,7 @@ function copyClipboard(elementID) {
 }
 
 // Function to dynamically get the userlist.
-async function getUsers(leaderboardName) {
+/* async */ function getUsers(leaderboardName) {
   let leaderboardID;
   let leaderboardHash;
   let gameId;
